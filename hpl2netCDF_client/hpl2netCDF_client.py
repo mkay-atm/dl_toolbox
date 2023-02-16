2cc115b5cf08b1eef0849cd31679cb1cc11f3af7
mkay-atm <58219143+mkay-atm@users.noreply.github.com>
2023-02-16 16:51:40 +0100

    Additional changes for bck-quicklook
    Updated the plotting routine to improve its speed and adjusted the colorscale for windcube backscatter plots to fall between 1e-8 and 1e-5.

--- hpl2netCDF_client/hpl2netCDF_client.py
+++ hpl2netCDF_client/hpl2netCDF_client.py
@@ -553,13 +553,16 @@ def find_num_dir(n_rays,calc_idx,azimuth,idx_valid):
             return np.all(check_num_dir(n_rays,calc_idx,azimuth,idx_valid)[0]), n_rays, check_num_dir(n_rays,calc_idx,azimuth,idx_valid)[1], check_num_dir(n_rays,calc_idx,azimuth,idx_valid)[2]
 ###################################################################################################
 ## helper functions for plotting
+def pbdist_alt(x_i,x_ip1,L):
+    return ((x_ip1-x_i) - L*np.rint((x_ip1-x_i) / L))
+
 def ql_helper(ds, confDict):
     # define limits for the range of backscatter values
     if confDict['SYSTEM'].lower() == 'halo':
         vmin, vmax= 1e-7, 1e-4
             
     if confDict['SYSTEM'].lower() == 'windcube':
-        vmin, vmax= 1e-9, 1e-4
+        vmin, vmax= 1e-8, 1e-6
         
     # use equal names        
     try:
@@ -574,48 +577,66 @@ def ql_helper(ds, confDict):
         azi = ds.azi.data
         beta = ds.beta.data
         time = ds.time
-        range_vec = ds.range.data
+        if len(list(ds.range.shape)) < 2:
+            range_vec =  ds.range.data
+        else:
+            range_vec =  ds.range.data[0]
         elevation = ds.elevation.data
     else:
         condi = True
-        azi = ds.where(ds.elevation<89, drop=True).azi.data
-        beta = ds.where(ds.elevation<89, drop=True).beta.data
-        time = ds.where(ds.elevation<89, drop=True).time
+        idt = np.where(ds.elevation.data<89)[0]
+        time = ds.time[idt]
+        azi = ds.azi.data[idt]
+        beta = ds.beta.data[idt]
         if len(list(ds.range.shape)) < 2:
-            range_vec =  ds.where(ds.elevation<89, drop=True).range.data
+            range_vec =  ds.range.data
         else:
-            range_vec =  ds.where(ds.elevation<89, drop=True).range.data[0]
-        elevation = ds.where(ds.elevation<89, drop=True).elevation.data
+            range_vec =  ds.range.data[idt][0]
+        elevation = ds.elevation.data[idt]
     # prepare data for plotting   
     # indentify maxima of each cycle
     if condi:
-        data = np.mod(azi-azi[0], 360)
-        index = np.where(abs(np.diff(data)) > 93)[0]
-        ## old method
-        # cycles = get_cycles(data, int(np.median(np.sign(np.diff(np.array(data)))))) 
-        ## new method
-        cycles = {}
-        start = 0
-        for ii,ind in enumerate(index):
-            if ind == index[-1]:
-                cycles.update( { ii: {'indices': np.arange(start, len(azi)), 'values': azi[start:len(azi)]} })
-                # print('finished cycling!')
-            else:
-                cycles.update( { ii: {'indices': np.arange(start, ind+1), 'values': azi[start:ind+1]} })
-                start = ind+1
-        df= pd.DataFrame.from_dict(cycles, orient='index')
-        df['indices'].apply(lambda row: len(row)).median()
+        #         data = np.mod(azi-azi[0], 360)
+#         index = np.where(abs(np.diff(data)) > 93)[0]
+#         ## old method
+#         # cycles = get_cycles(data, int(np.median(np.sign(np.diff(np.array(data)))))) 
+#         ## new method
+#         cycles = {}
+#         start = 0
+#         for ii,ind in enumerate(index):
+#             if ind == index[-1]:
+#                 cycles.update( { ii: {'indices': np.arange(start, len(azi)), 'values': azi[start:len(azi)]} })
+#                 # print('finished cycling!')
+#             else:
+#                 cycles.update( { ii: {'indices': np.arange(start, ind+1), 'values': azi[start:ind+1]} })
+#                 start = ind+1
+#         df= pd.DataFrame.from_dict(cycles, orient='index')
+#         df['indices'].apply(lambda row: len(row)).median()
 
 
+#         Z = beta
+#         mask= (np.isnan(Z)) | (Z==-999.)
+#         masked_Z = np.ma.masked_where(mask, Z)
+#         beta_max = np.empty((df.__len__(), beta.shape[1]))
+#         time_mean = np.empty((df.__len__()))
+
+#         for ii in range(df.__len__()):
+#             time_mean[ii] = time[df['indices'][ii]].mean(dim='time')
+#             beta_max[ii] = np.max(Z[df['indices'][ii]], axis=0)
         Z = beta
         mask= (np.isnan(Z)) | (Z==-999.)
         masked_Z = np.ma.masked_where(mask, Z)
-        beta_max = np.empty((df.__len__(), beta.shape[1]))
-        time_mean = np.empty((df.__len__()))
-
-        for ii in range(df.__len__()):
-            time_mean[ii] = time[df['indices'][ii]].mean(dim='time')
-            beta_max[ii] = np.max(Z[df['indices'][ii]], axis=0)
+        id_condi = np.round(pbdist_alt(azi[:-1].round() % 360, azi[1:].round() % 360, 180)) < 0
+        idx = np.where(np.hstack([True, id_condi]))[0]
+        beta_max = np.full((sum(id_condi), beta.shape[1]), np.nan)
+        time_mean = np.empty((sum(id_condi),))
+        for ii, (start, end) in enumerate(zip(idx[:-1], idx[1:])):
+            if end-start > 3:
+                time_mean[ii] = time[start:end].mean().data
+                beta_max[ii] = Z[start:end].max(axis=0)
+            else:
+                time_mean[ii] = time[start:end].mean().data
+                continue
 
     else:
         Z = beta
