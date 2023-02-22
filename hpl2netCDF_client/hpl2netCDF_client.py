51579252e2375f1807353cbde75bdf18563ee0f4
mkay-atm <58219143+mkay-atm@users.noreply.github.com>
2023-02-22 11:48:52 +0100

    Changed cycle id condition
    In order to identify sweep cycles within a scan such as VAD, I changed the condition in order to avoid all NaN output, when creating bck-quicklooks

--- hpl2netCDF_client/hpl2netCDF_client.py
+++ hpl2netCDF_client/hpl2netCDF_client.py
@@ -626,7 +626,8 @@ def ql_helper(ds, confDict):
         Z = beta
         mask= (np.isnan(Z)) | (Z==-999.)
         masked_Z = np.ma.masked_where(mask, Z)
-        id_condi = np.round(pbdist_alt(azi[:-1].round() % 360, azi[1:].round() % 360, 180)) < 0
+#         id_condi = np.round(pbdist_alt(azi[:-1].round() % 360, azi[1:].round() % 360, 180)) < 0
+        id_condi = np.round(azi[:-1].round() % 360 - azi[1:].round() % 360) >= 180
         idx = np.where(np.hstack([True, id_condi]))[0]
         beta_max = np.full((sum(id_condi), beta.shape[1]), np.nan)
         time_mean = np.empty((sum(id_condi),))
