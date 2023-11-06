db2ca6a69366dd23b19c8859b4bea3156dd124d7
rruefenacht <rruefenacht@users.noreply.github.com>
2023-11-06 17:35:08 +0100

    add script with example usage
    need to have this at project dir level, otherwise import of hpl2netCDF_client
    fails (possibly because of identical directory and filenames)

---
+++ example_usage.py
@@ -0,0 +1,42 @@
+import datetime
+
+from hpl2netCDF_client.hpl2netCDF_client import hpl2netCDFClient
+
+
+if __name__ == '__main__':
+    cmd = 'hpl_l1_l2'
+    conf = '/home/ruf/Desktop/dl_toolbox_test_data/config_ruf/wc_MeteoSwiss_Payerne_VAD_TP.conf'
+    date = datetime.datetime.strptime('2023-02-09', '%Y-%m-%d')
+
+    proc_dl = hpl2netCDFClient(conf, cmd, date)
+
+    proc_dl.display_config_dir()
+    proc_dl.display_configDict()
+
+    if cmd == 'hpl_l1':
+        proc_dl.dailylvl1()
+    if cmd == 'l1_l2':
+        proc_dl.dailylvl2()
+    if cmd == 'hpl_l1_l2':
+        proc_dl.dailylvl1()
+        proc_dl.dailylvl2()
+    if cmd == 'l1_l2wql':
+        proc_dl.dailylvl2()
+        proc_dl.lvl2ql()
+        proc_dl.bckql()
+    if cmd == 'hpl_l1_l2wql':
+        proc_dl.dailylvl1()
+        proc_dl.dailylvl2()
+        proc_dl.lvl2ql()
+        proc_dl.bckql()
+    if cmd == 'l1wql':
+        proc_dl.bckql()
+    if cmd == 'l2wql':
+        proc_dl.lvl2ql()
+    if cmd == 'hpl_l1_l2nrt':
+        proc_dl.nrtlvl1()
+        proc_dl.nrtlvl2()
+    if cmd == 'hpl_l2nrt':
+        proc_dl.nrtlvl1()
+        proc_dl.nrtlvl2()
+        proc_dl.rmlvl1()
